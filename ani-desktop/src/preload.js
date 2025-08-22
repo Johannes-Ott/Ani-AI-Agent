d21257685152001d5eb5231c6c48bda5058804cf
KIB0-1997 <johannes.e.ott@gmail.com>
2025-08-22 02:24:16 +0200

    Track ani-desktop files with Git LFS

--- ani-desktop/src/preload.js
+++ ani-desktop/src/preload.js
@@ -1,25 +1,3 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('ani', {
-  // windows
-  openUnity: () => ipcRenderer.invoke('window.openUnity'),
-  openN8n: (url) => ipcRenderer.invoke('open.n8n', url),
-  getEnv: () => ipcRenderer.invoke('env.get'),
-
-  // fs
-  listDir: (dir) => ipcRenderer.invoke('fs.listDir', dir),
-  readFile: (p) => ipcRenderer.invoke('fs.readFile', p),
-  writeFile: (p, content) => ipcRenderer.invoke('fs.writeFile', p, content),
-  mkDir: (p) => ipcRenderer.invoke('fs.mkDir', p),
-  openFolderDialog: () => ipcRenderer.invoke('fs.openFolderDialog'),
-  openFileDialog: () => ipcRenderer.invoke('fs.openFileDialog'),
-  saveFileDialog: (suggest) => ipcRenderer.invoke('fs.saveFileDialog', suggest),
-
-  // proc
-  runCommand: (cmd, args, cwd) => ipcRenderer.invoke('proc.run', cmd, args, cwd),
-  tryOpenUnityProject: (unityExe, projectPath) => ipcRenderer.invoke('unity.openProject', unityExe, projectPath),
-
-  // Settings + Workflow-Import
-  getSettings: () => ipcRenderer.invoke('settings:get'),
-  saveSettings: (obj) => ipcRenderer.invoke('settings:save', obj),
-  importWorkflows: (obj) => ipcRenderer.invoke('workflows:import', obj),
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:d9fe6a42e0d2e53afa15f502eb3709d7576d8230f350f7217ae5f582fc347add
+size 1186
